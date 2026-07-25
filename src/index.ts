0fc7f2031359bdfb849eea6f9af7283f9fce0abd
Amit Yadav <amity7205@gmail.com>
2026-07-26 01:21:17 +0530

    fix:prisma 7 import issue

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import express from "express";
+import { client } from "./db";
 
 const app=express();
 app.use(express.json());
 
+app.post('/',async(req,res)=>{
+const user=await client.user.create({
+    data:{
+        name:Math.random.toString(),
+        password:Math.random.toString()
+    }
+})
+res.json({
+    message:"user is created",
+    user
+})
+})
+app.get("/",async (req,res)=>{
+    const user=await client.user.findFirst();
+    res.json({
+        message:"hello",
+        user
+    })
+})
 app.listen(3000,()=>{
     console.log("server is running on 3000");
 })
